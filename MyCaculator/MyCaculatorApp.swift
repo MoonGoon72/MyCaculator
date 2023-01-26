c6016c7407f3cd9fd028e21a0df0c2c49aef3eaf
MoonGoon <moongoon.cnu@gmail.com>
2023-01-26 17:01:04 +0900

    Initial Commit

---
+++ MyCaculator/MyCaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCaculatorApp.swift
+//  MyCaculator
+//
+//  Created by cnu on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MyCaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

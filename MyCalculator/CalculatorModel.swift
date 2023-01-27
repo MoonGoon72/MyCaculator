c194ab8c63bd4f60f9b36e4046cd3812dbd20e08
MoonGoon <moongoon.cnu@gmail.com>
2023-01-27 19:49:04 +0900

    refactor Calculator

--- MyCalculator/CalculatorModel.swift
+++ MyCalculator/CalculatorModel.swift
@@ -7,30 +7,132 @@
 
 import Foundation
 
-class CalculatorModel {
+func chageSign(operand: Double) -> Double {
+    return -operand
+}
+
+func multiply(op1: Double,op2: Double) -> Double {
+    return op1 * op2
+}
+
+
+class CalculatorModel: ObservableObject {
     
-    func addNum (_ first:Int, _ second:Int) -> Int {
-        return first + second
+    @Published var displayValue:String = "0"
+    
+    private let buttonCodeVertical: [[String]] = [
+        ["C", "±", "%", "÷"],
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "="]
+    ]
+    
+    private let buttonCodeHorizontal: [[String]] = [
+        ["(",   ")",    "mc",   "m+",   "m-",   "mr",   "C",    "±",    "%",    "÷"],
+        ["2nd", "x²",   "x³",   "xʸ",   "eˣ",   "10ˣ",  "7",    "8",    "9",    "×"],
+        ["√x",  "2/x",  "3/x",  "y/x",  "ln",   "log10","4",    "5",    "6",    "−"],
+        ["x!",  "sin",  "cos",  "tan",  "e",    "EE",   "1",    "2",    "3",    "+"],
+        ["Rad", "sinh", "cosh", "tanh", "pi",   "Rand", "0",    "0",    ".",    "="]
+    ]
+    
+    func getButtonCodeList() -> [[String]] {
+        return buttonCodeVertical
+    }
+    func inputToken(input:String) {
+        
+        if let _ = Int(input) {
+            inputDigit(input: input)
+        }
+        else {
+            if userIsInTheMiddleOfTyping, let value =
+                Double(displayValue) {
+                setOperand(operand: value)
+                userIsInTheMiddleOfTyping = false
+            }
+            performOperation(input)
+            if let value = result {
+                displayValue = String(value)
+            }
+        }
+    }
+    private var userIsInTheMiddleOfTyping = false
+    
+    private enum Operation {
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
     }
     
-    func subNum (_ first:Int, _ second:Int) -> Int {
-        return first - second
+    private var operations: Dictionary<String, Operation> = [
+        "±" : Operation.unaryOperation(chageSign),
+        "%" : Operation.unaryOperation({ $0 / 100.0 }),
+        "×" : Operation.binaryOperation({ $0 * $1 }),
+        "+" : Operation.binaryOperation({ $0 + $1 }),
+        "-" : Operation.binaryOperation({ $0 - $1 }),
+        "÷" : Operation.binaryOperation({ $0 / $1 }),
+        "=" : Operation.equals
+    ]
+    
+    
+    
+    private func inputDigit(input:String) {
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyinDisplay = displayValue
+            displayValue = textCurrentlyinDisplay + input
+        } else {
+            displayValue = input
+            userIsInTheMiddleOfTyping = true
+        }
     }
     
-    func divNum (_ first:Int, _ second:Int) -> Int {
-        return first / second
+    private var accumulator: Double?
+    
+    private func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pbo = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            }
+        }
     }
     
-    func multNum (_ first:Int, _ second:Int) -> Int {
-        return first * second
+    private func setOperand(operand: Double) {
+        accumulator = operand
     }
     
-    func oppositeNum (_ first:Int) -> Int {
-        return -first
+    private var result: Double? {
+        get {
+            return accumulator
+        }
     }
     
-    func percentNum (_ first:Int, _ second:Int) -> Int {
-        return first + second
+    private func performPendingBinaryOperation() {
+        if let pend = pbo, let accum = accumulator {
+            accumulator = pend.perform(with: accum)
+            pbo = nil
+        }
+    }
+    
+    
+    private var pbo: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand : Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
     }
     
     func numberFormatter(number: Int) -> String {
@@ -43,7 +145,7 @@ class CalculatorModel {
     func stringFormatter(string: String) -> Any {
         let numberFormatter: NumberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
-
+        
         return numberFormatter.number(from: string)!
     }
 }
--- MyCalculator/CalculatorView.swift
+++ MyCalculator/CalculatorView.swift
@@ -1,74 +1,5 @@
 import SwiftUI
 
-enum ButtonType: String {
-    case first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, zero
-    case dot, equal, plus, minus, multiply, divide
-    case clear, opposite, percentage
-    
-    var buttonDisplayName: String {
-        switch self {
-        case .first:
-            return "1"
-        case .second:
-            return "2"
-        case .third:
-            return "3"
-        case .fourth:
-            return "4"
-        case .fifth:
-            return "5"
-        case .sixth:
-            return "6"
-        case .seventh:
-            return "7"
-        case .eighth:
-            return "8"
-        case .nineth:
-            return "9"
-        case .zero:
-            return "0"
-        case .dot:
-            return "."
-        case .equal:
-            return "="
-        case .plus:
-            return "+"
-        case .minus:
-            return "-"
-        case .multiply:
-            return "X"
-        case .divide:
-            return "÷"
-        case .clear:
-            return "C"
-        case .opposite:
-            return "±"
-        case .percentage:
-            return "%"
-        }
-    }
-    
-    var backgroundColor: Color {
-        switch self {
-        case .first,.second,.third,.fourth,.fifth,.sixth,.seventh,.eighth,.nineth,.zero,.dot:
-            return Color("NumberButton")
-        case .equal, .plus, .minus, .multiply, .divide:
-            return Color.orange
-        case .clear, .opposite, .percentage:
-            return Color.gray
-        }
-    }
-    
-    var foregroundColor: Color {
-        switch self {
-        case .first,.second,.third,.fourth,.fifth,.sixth,.seventh,.eighth,.nineth,.zero,.dot, .equal, .plus, .minus, .multiply, .divide:
-            return Color.white
-        case .clear, .opposite, .percentage:
-            return Color.black
-        }
-    }
-}
-
 extension Int {
     public func toStringWithComma() -> String? {
         let numberFormmater = NumberFormatter()
@@ -101,128 +32,42 @@ extension NSNumber {
 
 
 struct CalculatorView: View {
+    @ObservedObject private var brain:CalculatorModel = CalculatorModel()
     
     @State private var result: String = "0"
-    @State var tmpNumber: Int = 0
     @State var calculator: CalculatorModel = CalculatorModel()
-    @State var operatorType: ButtonType = .clear
-    @State var isNotEditing: Bool = true
     
-    private let buttonData: [[ButtonType]] = [
-        [.clear, .opposite, .percentage, .divide],
-        [.seventh, .eighth, .nineth, .multiply],
-        [.fourth, .fifth, .sixth, .minus],
-        [.first, .second, .third, .plus],
-        [.zero, .dot, .equal],
-    ]
+    func inputToken(token:String) {
+        brain.inputToken(input: token)
+    }
     var Buttons: some View {
-        ForEach(buttonData, id: \.self) { line in
+        ForEach(Array(brain.getButtonCodeList().enumerated()), id: \.offset) { vIdx, line in
             HStack {
-                ForEach(line, id: \.self) { item in
-                    Button {
-                        if isNotEditing {
-                            if item == .clear {
-                                result = "0"
-                                isNotEditing = true
-                            } else if
-                                item == .plus ||
-                                    item == .minus ||
-                                    item == .divide ||
-                                    item == .multiply ||
-                                    item == .percentage ||
-                                    item == .opposite
-                            {
-                                result = "Error"
-                            } else {
-                                result = item.buttonDisplayName
-                                isNotEditing = false  // 이미 입력 받고 있음
-                            }
-                            isNotEditing = false
-                        } else {
-                            if item == .clear {
-                                isNotEditing = true
-                                result = "0"
-                            }
-                            // 단위수 콤마가 들어가면 타입캐스팅 오류로 0이 되어 이 부분 수정
-                            else if item == .plus {
-                                tmpNumber = (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)
-                                operatorType = .plus
-                                isNotEditing = true  // 새로 입력받기 위함
-                            } else if item == .minus {
-                                tmpNumber = (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)
-                                operatorType = .minus
-                                isNotEditing = true
-                            } else if item == .multiply {
-                                tmpNumber = (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)
-                                operatorType = .multiply
-                                isNotEditing = true
-                            } else if item == .divide {
-                                tmpNumber = (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)
-                                operatorType = .divide
-                                isNotEditing = true
-                            } else if item == .opposite {
-                                tmpNumber = (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)
-                                operatorType = .opposite
-                                result = String(calculator.oppositeNum(Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0))
-                            } else if item == .equal {
-                                switch operatorType {
-                                case .plus:
-                                    result = calculator.addNum((Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0), tmpNumber).toStringWithComma() ?? "0"
-                                case .minus:
-                                    result = calculator.subNum(tmpNumber, (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)).toStringWithComma() ?? "0"
-                                    // 먼저 써놨던 수에서 나중 수를 빼야하니까!
-                                case .multiply:
-                                    result = calculator.multNum((Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0), tmpNumber).toStringWithComma() ?? "0"
-                                case .divide:
-                                    result = calculator.divNum(tmpNumber, (Int(result.toIntNoneComma()?.toString() ?? "0") ?? 0)).toStringWithComma() ?? "0"
-                                case .percentage:
-                                    result = result
-                                default:
-                                    result = result
-                                }
-                            } else {
-                                if result == "0" {
-                                    result = item.buttonDisplayName
-                                } else {
-                                    result = result.toIntNoneComma()?.toString() ?? "777"
-                                    result += item.buttonDisplayName
-                                    result = (Int(result) ?? 0).toStringWithComma() ?? "0"
-                                }
-                            }
-                        }
-                    } label: {
-                        Text(item.buttonDisplayName)
-                            .bold()
-                            .frame(width: calculateButtonWidth(button: item),height: calculateButtonHeigth(button: item))
-                            .background(item.backgroundColor)
-                            .cornerRadius(40).foregroundColor(item.foregroundColor)
-                            .font(.system(size: 30))
+                let hLast = line.count - 1
+                ForEach(Array(line.enumerated()), id: \.offset) { hIdx, buttonTitle in
+                    Button(action: {inputToken(token: buttonTitle)}) {
+                        Text(buttonTitle)
+                            .frame(width: (buttonTitle == "0" ? 170 : 80), height: 80)
+                            .background(hIdx  == hLast ? .orange : vIdx == 0 ? .gray : .secondary)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
                     }
+                    
                 }
             }
         }
     }
     
-    private func calculateButtonWidth(button buttonType: ButtonType) -> CGFloat {
-        switch buttonType {
-        case .zero:
-            return (UIScreen.main.bounds.width - 5*10) / 4 * 2.1
-        default:
-            return ((UIScreen.main.bounds.width - 5*10) / 4)
-        }
-        // (전체너비 - 5*10)/4
-    }
-    private func calculateButtonHeigth(button buttonType: ButtonType) -> CGFloat {
-        return (UIScreen.main.bounds.width - 5*10) / 4
-    }
-    
     var Result: some View {
         HStack {
             Spacer()
-            Text(result)
+            Text(brain.displayValue)
                 .padding(10)
                 .font(.system(size: 75))
                 .foregroundColor(Color.white)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
         }
     }
     
